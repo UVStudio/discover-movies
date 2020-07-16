b2255e724329fdeb61c5b366bbc83f21d13984e0
Leonard Shen <uvstudio2199@gmail.com>
2020-07-15 20:16:11 -0400

    installed cra

---
+++ build/client/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var react_dom_1 = __importDefault(require("react-dom"));
+var App = function () {
+    return (<div>
+      <h2>Hello React</h2>
+    </div>);
+};
+react_dom_1.default.render(<App />, document.querySelector('#root'));
---
+++ build/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var moviesData_1 = require("./routes/moviesData");
+var body_parser_1 = __importDefault(require("body-parser"));
+var app = express_1.default();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(moviesData_1.router);
+var PORT = process.env.PORT || 5000;
+app.listen(PORT, function () { return console.log("Listening to port " + PORT); });
