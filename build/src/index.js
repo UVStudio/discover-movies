8334fb5faeb37841ee57ff499ca442efbe1ca48a
Leonard Shen <uvstudio2199@gmail.com>
2020-07-25 21:40:38 -0400

    updated index.ts to include production

--- build/src/index.js
+++ build/src/index.js
@@ -5,10 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var moviesData_1 = require("./routes/moviesData");
+var path_1 = __importDefault(require("path"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var app = express_1.default();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(moviesData_1.router);
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express_1.default.static('client/build'));
+    app.get('*', function (req, res) {
+        res.sendFile(path_1.default.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, function () { return console.log("Listening to port " + PORT); });
 ///notes
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import { router } from './routes/moviesData';
-
+import path from 'path';
 import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
